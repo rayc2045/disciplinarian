f67af320a922e8bd33d40b207c1fc5dd58eb5eda
rayc2045 <>
2023-02-15 01:00:15 +0800

    Fix complete progress

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,35 +6,38 @@ import items from './esm/items.js';
 const App = {
   init() {
     for (const item of items) {
-      item.steps = item.step
+      item.steps = item.steps
         .split('，')
         .map(step => ({ step, completed: false }));
-      delete item.step;
     }
   },
   completeStep(item, id) {
-    // toggle complete
     if (document.querySelector('canvas')) return;
     if (id > 0 && !item.steps[id - 1].completed) return;
+
     if (
       id !== item.steps.length - 1 &&
       item.steps[id].completed &&
       !item.steps[id + 1].completed
     )
       return (item.steps[id].completed = false);
-    // one completed
+
     item.steps[id].completed = true;
-    item.progress = utils.getPercentage(
-      item.steps.filter(step => step.completed).length,
-      item.steps.length
-    );
-    // all completed
+
     if (item.steps.every(step => step.completed)) {
       confetti();
       setTimeout(() => {
         for (const step of item.steps) step.completed = false;
-        item.progress = 0;
       }, 3000);
+    }
+  },
+  setProgress() {
+    if (typeof items[0].steps === 'string') return;
+    for (const item of items) {
+      item.progress = utils.getPercentage(
+        item.steps.filter(step => step.completed).length,
+        item.steps.length
+      );
     }
   },
 };
