fb9cf048d62cc70edfdfe2b5a001e213828041ae
rayc2045 <>
2023-02-15 00:22:08 +0800

    Add percentage

---
+++ src/scripts/esm/utils.js
@@ -0,0 +1,5 @@
+export default {
+  getPercentage(num1, num2) {
+    return Math.floor((num1 / num2) * 100);
+  },
+};
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,9 +1,9 @@
 import { createApp } from 'https://esm.sh/petite-vue';
 import confetti from 'https://esm.sh/canvas-confetti';
+import utils from './esm/utils.js';
 import items from './esm/items.js';
 
 const App = {
-  items,
   init() {
     for (const item of items) {
       item.steps = item.step
@@ -13,6 +13,7 @@ const App = {
     }
   },
   completeStep(item, id) {
+    // toggle complete
     if (document.querySelector('canvas')) return;
     if (id > 0 && !item.steps[id - 1].completed) return;
     if (
@@ -21,14 +22,21 @@ const App = {
       !item.steps[id + 1].completed
     )
       return (item.steps[id].completed = false);
+    // one completed
     item.steps[id].completed = true;
+    item.progress = utils.getPercentage(
+      item.steps.filter(step => step.completed).length,
+      item.steps.length
+    );
+    // all completed
     if (item.steps.every(step => step.completed)) {
       confetti();
       setTimeout(() => {
         for (const step of item.steps) step.completed = false;
+        item.progress = 0;
       }, 3000);
     }
   },
 };
 
-createApp(App).mount();
+createApp({ ...App, items, utils }).mount();
