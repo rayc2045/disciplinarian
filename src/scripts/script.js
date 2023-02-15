d1c1cd01592e7c53d7ee4ee3f83a2f5dcfaf7819
rayc2045 <>
2023-02-15 12:47:49 +0800

    Add complete times, optimize cursor disable

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,12 +4,14 @@ import utils from './esm/utils.js';
 import items from './esm/items.js';
 
 const App = {
-  isDisabled: false,
   init() {
     for (const item of items) {
-      item.steps = item.steps
-        .split('，')
-        .map(step => ({ step, completed: false }));
+      item.completeTimes = 0;
+      item.steps = item.steps.split('，').map(step => ({
+        step,
+        completed: false,
+        editable: false,
+      }));
     }
   },
   update() {
@@ -19,27 +21,38 @@ const App = {
         item.steps.filter(step => step.completed).length,
         item.steps.length
       );
+      // set editable
+      for (const stepId in item.steps) item.steps[stepId].editable = false;
+      const completedStepsNum = item.steps.filter(
+        step => step.completed
+      ).length;
+      const lastCompleteId = completedStepsNum - 1;
+      if (completedStepsNum === item.steps.length) item.steps[lastCompleteId].editable = false;
+      else if (lastCompleteId < 0) item.steps[0].editable = true;
+      else {
+        if (lastCompleteId > 1) item.steps[lastCompleteId - 1].editable = false;
+        item.steps[lastCompleteId].editable = true;
+        if (completedStepsNum < item.steps.length)
+          item.steps[lastCompleteId + 1].editable = true;
+      }
     }
   },
   completeStep(item, id) {
-    if (this.isDisabled) return;
-    if (id > 0 && !item.steps[id - 1].completed) return;
-
-    if (
-      id !== item.steps.length - 1 &&
-      item.steps[id].completed &&
-      !item.steps[id + 1].completed
-    )
-      return (item.steps[id].completed = false);
-
-    item.steps[id].completed = true;
+    // if (id > 0 && !item.steps[id - 1].completed) return;
+    // if (
+    //   id !== item.steps.length - 1 &&
+    //   item.steps[id].completed &&
+    //   !item.steps[id + 1].completed
+    // )
+    //   return (item.steps[id].completed = false);
+    if (item.steps[id].editable)
+      item.steps[id].completed = !item.steps[id].completed;
 
     if (item.steps.every(step => step.completed)) {
-      this.isDisabled = true;
       confetti();
       setTimeout(() => {
         for (const step of item.steps) step.completed = false;
-        this.isDisabled = false;
+        item.completeTimes++;
       }, 3000);
     }
   },
