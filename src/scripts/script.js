3175bebccef9a1831aaa290942a72bab2483dea1
rayc2045 <>
2023-02-16 20:47:59 +0800

    Update todo data

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,8 @@
 import { createApp, reactive } from 'https://esm.sh/petite-vue';
 import confetti from 'https://esm.sh/canvas-confetti';
 import utils from './esm/utils.js';
-import todo from './esm/todo.js';
-/*
-todo = [
-  {
-    title: '',
-    tasks: [ '...', '...', '...', ]
-  },
-]
-*/
 
-const items = reactive(todo);
+const items = reactive([]);
 /*
 items = [
   {
@@ -19,6 +10,7 @@ items = [
     open: false,
     completeTimes: 0,
     progress: 0,
+    descriptions: []
     tasks: [
       { task: '...', completed: false, editable: false },
       { task: '...', completed: false, editable: false },
@@ -29,7 +21,18 @@ items = [
 */
 
 const App = {
-  init() {
+  async init() {
+    const txt = await fetch('/data/todo.txt').then(res => res.text());
+    for (const paragraph of txt.split('\n\n')) {
+      const data = { title: '', descriptions: [], tasks: [] };
+      paragraph.split('\n').forEach((p, idx) => {
+        if (idx === 0) return (data.title = p);
+        if (p.startsWith('-'))
+          return data.descriptions.push(p.replace('- ', ''));
+        data.tasks.push(p);
+      });
+      items.push(data);
+    }
     const { open } = utils.getQueryParams();
     for (const item of items) {
       item.open = typeof open !== 'undefined';
@@ -42,8 +45,8 @@ const App = {
     }
   },
   update() {
-    if (typeof items[0].tasks[0] === 'string') return;
     for (const item of items) {
+      // set progress
       const completedTasksNum = item.tasks.filter(
         task => task.completed
       ).length;
@@ -64,10 +67,12 @@ const App = {
     const task = item.tasks[taskId];
     if (task.editable) task.completed = !task.completed;
     if (item.tasks.every(task => task.completed)) {
+      item.tasks.at(-1).editable = false;
       this.confetti(3);
       setTimeout(() => {
         item.completeTimes++;
         for (const task of item.tasks) task.completed = false;
+        item.tasks[0].editable = true;
       }, 3000);
     }
   },
