18117648650f4bbc81bd6f911d73acf58b3fc5c5
rayc2045 <>
2023-02-21 21:38:13 +0800

    Update fetch txt function and file root

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,20 +3,21 @@ import confetti from 'https://esm.sh/canvas-confetti';
 import utils from './esm/utils.js';
 import style from './esm/style.js';
 import query from './esm/query.js';
-
 import storage from './esm/localStorage.js';
-const STORAGE_KEY = 'discipline-todo';
+
+const STORAGE_KEY = `discipline-todo-${query.file}`;
 
 const items = reactive([]);
 
 const App = {
+  errorMessage: '',
   async init() {
     if (!query.isSave) {
-      await this.parseTxt();
+      await this.parseTxt(query.file);
       return storage.delete(STORAGE_KEY);
     }
     const store = storage.fetch(STORAGE_KEY);
-    if (!store.length) await this.parseTxt();
+    if (!store.length) await this.parseTxt(query.file);
     else {
       for (const storeItem of store) {
         if (query.isOpen) storeItem.open = true;
@@ -28,11 +29,11 @@ const App = {
     }
     storage.save(STORAGE_KEY, items);
   },
-  async parseTxt() {
-    const txt = await fetch('/data/todo.txt').then(res => res.text());
+  async parseTxt(path) {
+    const txt = await this.fetchTxt(path);
 
     for (const paragraph of txt.split('\n\n')) {
-      if (paragraph.startsWith('//')) continue;
+      if (!paragraph.trim() || paragraph.startsWith('//')) continue;
       const data = { title: '', descriptions: [], tasks: [] };
       paragraph.split('\n').forEach((line, lineIdx) => {
         if (lineIdx === 0) return (data.title = line);
@@ -54,6 +55,38 @@ const App = {
         })),
       });
     }
+  },
+  async fetchTxt(path) {
+    const root = '/public/txt';
+
+    let txtPath = '';
+    if (path.startsWith('http'))
+      txtPath = path
+        .split(' ')
+        .join('+')
+        .replace('+', '?')
+        .replaceAll('+', '&');
+    else {
+      if (!path.startsWith(root) || !path.startsWith(root.replace('/', '')))
+        txtPath += root;
+      if (!path.startsWith('/')) txtPath += '/';
+      txtPath += path;
+      txtPath = txtPath.replace(root.repeat(2), root);
+      if (!txtPath.endsWith('.txt')) txtPath += '.txt';
+    }
+
+    let res;
+    try {
+      res = await fetch(txtPath);
+    } catch (error) {
+      this.errorMessage = '檔案抓取失敗';
+      res = await fetch(`${root}/example.txt`);
+    }
+    if (!res.ok) {
+      this.errorMessage = '找不到檔案';
+      res = await fetch(`${root}/example.txt`);
+    }
+    return await res.text();
   },
   toggleOpen(item, open = !item.open) {
     item.open = open;
