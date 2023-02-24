f252d9c5c23effe1ee862985b2bd38b253d5e072
rayc2045 <>
2023-02-24 14:51:45 +0800

    Update style module

--- src/scripts/esm/style.js
+++ src/scripts/esm/style.js
@@ -1,5 +1,10 @@
 export default {
   group(style, to) {
-    return `{${style}}${to}`.replaceAll('\n', ' ').replaceAll(' ', ';');
+    return `{${style
+      .trim()
+      .split(' ')
+      .filter(s => s.length)
+      .map(s => s.replaceAll('\n', ''))
+      .join(';')}}${to}`;
   },
 };
