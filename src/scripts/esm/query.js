2e9b8b956f343968a40829ff2e4311215ace3247
rayc2045 <>
2023-03-12 02:40:21 +0800

    Update query.js

--- src/scripts/esm/query.js
+++ src/scripts/esm/query.js
@@ -1,17 +1,18 @@
 import utils from './utils.js';
 const param = utils.getQueryParams();
+const queries = Object.keys(param).map(key => key.toLocaleLowerCase());
 
 export default {
   file: param.file,
-  isDark: param.hasOwnProperty('dark') && !param.hasOwnProperty('light'),
-  isLight: param.hasOwnProperty('light') && !param.hasOwnProperty('dark'),
-  is3D: param.hasOwnProperty('3D') || param.hasOwnProperty('3d'),
-  isSave: param.hasOwnProperty('autosave'),
-  isOpen: param.hasOwnProperty('open') && !param.hasOwnProperty('autoclose'),
-  isClose: param.hasOwnProperty('autoclose'),
-  isList: param.hasOwnProperty('list') && !param.hasOwnProperty('checkbox'),
-  isOrdered: !param.hasOwnProperty('unordered'),
-  isOutlined: param.hasOwnProperty('outlined') && !param.hasOwnProperty('filled'),
-  isStrict: !param.hasOwnProperty('free'),
-  isCycle: param.hasOwnProperty('cycle'),
+  isDark: queries.includes('dark') && !queries.includes('light'),
+  isLight: queries.includes('light') && !queries.includes('dark'),
+  is3D: queries.includes('3d'), // param.hasOwnProperty('3D')
+  isSave: queries.includes('autosave'),
+  isOpen: queries.includes('open') && !queries.includes('autoclose'),
+  isClose: queries.includes('autoclose'),
+  isList: queries.includes('list') && !queries.find(query => query.startsWith('check')),
+  isOrdered: !queries.includes('unordered'),
+  isOutlined: queries.includes('outlined') && !queries.includes('filled'),
+  isStrict: !queries.includes('free'),
+  isCycle: queries.includes('cycle'),
 };
